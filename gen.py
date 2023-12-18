6bcac03b75592715b48d1f888608eac6e83d1b1d
hhhaaahhhaa <thomas1232121@gmail.com>
2023-12-18 22:52:16 +0800

    do not use batch encoding (bugged)

--- gen.py
+++ gen.py
@@ -47,34 +47,32 @@ for k, v in dataset.items():
         continue
     if "train" not in k:
         continue
+    if "truthfulqa" not in k:
+        continue
     print(k)
     for data_item in tqdm(v): 
         q = data_item['question']
         if 'truthfulqa' in k or 'hotpotqa' in k:
             ans_prompt = f'Answer the question with yes or no. Question: {q}\nOutput:'
-        elif 'strategyqa' in k:
+        if 'strategyqa' in k:
             ans_prompt = f'Answer the question with True or False. Question: {q}\nOutput:'
-        elif 'openbookqa' in k:
+        if 'openbookqa' in k:
             ops_string = "\n".join(data_item['options'])
             ans_prompt = f'Answer the question with Options:\n{ops_string}. Question: {q}\nOutput:'
-
         data_item['inst'] = defaultdict(dict)
 
-        # Prepare all prompts for batch processing
-        prompts = [f"Instruct: {i} For the Question: {q}\nOutput:" for i in actions_str]
-
-        # Perform batch processing
-        inputs = tokenizer.batch_encode_plus(prompts, return_tensors="pt", return_attention_mask=True, padding=True, truncation=True)
-        output_ids = model.generate(**inputs, max_length=512, eos_token_id=tokenizer.eos_token_id)
-        actual_seq_lengths = inputs.attention_mask.sum(dim=1)
-        texts = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-
-        # Post-process each item in the batch
-        for i, text in zip(actions_str, texts):
+        for i in actions_str:
+            prompt = f"Instruct: {i} For the Question: {q}\nOutput:"
+            inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=True)
+            output_ids = model.generate(**inputs, max_length=512, eos_token_id=tokenizer.eos_token_id)
+            actual_seq_lengths = inputs.attention_mask.sum(dim=1)
+            output_ids = [output_id[seq_length:] for output_id, seq_length in zip(output_ids, actual_seq_lengths)]
+            text = tokenizer.batch_decode(output_ids, skip_special_tokens=True,)[0]
             prompt = f'Instruct: With Consider that {i}: {text}. {ans_prompt}'
-            scores = llm.score_choice(prompt, data_item['options'])
-            data_item['inst'][i] = {'step': text, 'score': list(scores)}
+            scores = llm.score_choice(prompt,data_item['options'])
+            data_item['inst'][i] = {'step':text,'score':list(scores)}
+        print(data_item)
         processed_data.append(data_item)
-
+    
     with open(f'./final_data/{k}_processed_data.json', 'w') as json_file:
         json.dump(processed_data, json_file, indent=4)
